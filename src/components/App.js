70adc5a7d9260334ba9eebc6a31f9827e7636a07
Dmytro <dmvs.9898@gmail.com>
2022-02-20 16:52:48 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
-// import user from './user.json';
+import user from './user.json';
 import data from './statistic/data.json';
 import friends from './FriendList/friends.json';
 import transactions from './transaction/transactions.json';
 
-// import Profile from './Profile';
+import Profile from './Profile';
 import Statistic from './statistic/Statistic';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './transaction/TransactionHistory';
@@ -11,13 +11,13 @@ import TransactionHistory from './transaction/TransactionHistory';
 export default function App() {
   return (
     <div>
-      {/* <Profile
+      <Profile
         username={user.username}
         tag={user.tag}
-        location={user.location}    
+        location={user.location}
         avatar={user.avatar}
         stats={user.stats}
-      /> */}
+      />
 
       <Statistic title="Upload stats" items={data} />
       <FriendList friends={friends} />
